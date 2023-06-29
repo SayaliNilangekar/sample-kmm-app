7f023b316824599be6944b167a5b050fc13fa0dd
Sayali Nilangekar <sayalisn4@gmail.com>
2023-06-29 08:50:46 -0700

    Fixes

--- iosBuildFixApp/shared/src/commonMain/kotlin/com/example/iosbuildfixapp/AppScreen.kt
+++ iosBuildFixApp/shared/src/commonMain/kotlin/com/example/iosbuildfixapp/AppScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.ui.Modifier
@@ -26,54 +27,80 @@ fun AppScreen() {
 
     var myData by remember { mutableStateOf(listOf(myResponse("", "Loading...", "Loading...", 0))) }
 
-    Box(
-        Modifier.paddingFromBaseline(top = 50.dp),
-        contentAlignment = Alignment.TopCenter
-    ) {
-        Text(Title().title(), color = Color.Black, fontSize = 30.sp, textAlign = TextAlign.Center)
-    }
+    Column(Modifier.fillMaxSize()) {
 
-    Box(
-        Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
+        Box(
+            Modifier.weight(1f).padding(vertical = 10.dp, horizontal = 5.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Text(
+                Title().title(),
+                color = Color.Black,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center
+            )
+        }
 
-        Text(myData.firstOrNull()?.setup ?: "", modifier = Modifier.paddingFromBaseline(bottom = 200.dp).padding(horizontal = 15.dp), color = Color.Black, fontSize = 25.sp, textAlign = TextAlign.Center)
-        Text(myData.firstOrNull()?.punchline ?: "", modifier = Modifier.paddingFromBaseline(top = 100.dp).padding(horizontal = 15.dp), color = Color.Black, fontSize = 25.sp, textAlign = TextAlign.Center)
-    }
+        Box(
+            Modifier.weight(1f).align(Alignment.CenterHorizontally),
+            contentAlignment = Alignment.Center
+        ) {
 
-    Box(
-        Modifier.paddingFromBaseline(bottom = 50.dp),
-        contentAlignment = Alignment.BottomCenter
-    ) {
+            Text(
+                myData.firstOrNull()?.setup ?: "",
+                modifier = Modifier.paddingFromBaseline(bottom = 200.dp)
+                    .padding(horizontal = 15.dp),
+                color = Color.Black,
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                myData.firstOrNull()?.punchline ?: "",
+                modifier = Modifier.paddingFromBaseline(top = 100.dp).padding(horizontal = 15.dp),
+                color = Color.Black,
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
+            )
+        }
 
-        val scope = rememberCoroutineScope()
+        Box(
+            Modifier.weight(1f).align(Alignment.CenterHorizontally).padding(vertical = 10.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
 
-        Button(
-            onClick = {
-                // Call api here to get new joke
-                scope.launch {
-                    myData = try {
-                        println("In try block!")
-                        val response = getResponse()
-                        println("myData: " + myData)
-                        response
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        listOf(myResponse("", "", "", 0))
+            val scope = rememberCoroutineScope()
+
+            Button(
+                onClick = {
+                    // Call api here to get new joke
+                    scope.launch {
+                        myData = try {
+                            println("In try block!")
+                            val response = getResponse()
+                            println("myData: " + myData)
+                            response
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            listOf(myResponse("", "", "", 0))
+                        }
                     }
                 }
+            ) {
+                Text(
+                    "Get new joke",
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
             }
-        ) {
-            Text("Get new joke", color = Color.White, fontSize = 20.sp, textAlign = TextAlign.Center)
         }
-    }
-    LaunchedEffect(true) {
-        try {
-            myData = getResponse()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            myData = listOf(myResponse("", "", "", 0))
+        LaunchedEffect(true) {
+            try {
+                myData = getResponse()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                myData = listOf(myResponse("", "", "", 0))
+            }
         }
     }
 }
